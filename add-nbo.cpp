1873ddef30194d590f60d3adaf6405dbcbfa3e9c
comalmot <daesul2@naver.com>
2020-07-14 00:42:32 +0900

    fread() 를 통해서 숫자 얻기에 성공! 이제 이제 더하기만 하면 된다

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,60 +1,33 @@
 #include <stddef.h> // for size_t
 #include <stdint.h> // for uint8_t
 #include <stdio.h> // for printf
+#include <string.h>
 #include <netinet/in.h>
 
-void dump(void* p, size_t n) {
-	uint8_t* u8 = static_cast<uint8_t*>(p);
-	size_t i = 0;
-	while (true) {
-		printf("%02X ", *u8++);
-		if (++i >= n) break;
-		if (i % 8 == 0)
-			printf("\n");
-	}
-	printf("\n");
-}
-
-void write_4660() {
-	uint16_t port = 4660; // 0x1234
-	printf("port number = %d\n", port);
-	dump(&port, sizeof(port));
-}
-
-uint16_t my_ntohs(uint16_t n) {
-	//uint16_t n1 = (n & 0xFF00) >> 8;
-	// 0x0034
+int main(int argc, char *argv[]) {
 	
-	//uint16_t n2 = (n & 0x00FF) << 8;
-	// 0x1200
+	FILE *fp1 = fopen(argv[1], "rb");
+	FILE *fp2 = fopen(argv[2], "rb");
 
-	//return (n2 | n1);
-	//
+	char buf1[4] = {'\0', };
+	char buf2[4] = {'\0', };
+
+
+	uint32_t input1 = 0x0;
+	uint32_t input2 = 0x0;
+
+	fread(&input1, sizeof(uint32_t), 1, fp1);
+	fread(&input2, sizeof(uint32_t), 1, fp2);
 	
-	return (n & 0xFF00) >> 8 | (n & 0x00FF) << 8;
-}
-
-void  write_0x1234() {
-	uint8_t network_buffer[] = { 0x12, 0x34 };
-	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	//uint16_t n = *p; // TODO
+	//input1 = (uint32_t)buf1
+	uint64_t result = 0;
 	
-	uint16_t n = ntohs(*p); //my_ntohs(*p);
-	printf("16 bit number=0x%x\n", n);
-}
+	//printf("0x%X\n", buf1);
+	
+	printf("input1 is : %u\n", ntohl(input1));
+	printf("input2 is : %u\n", ntohl(input2));
 
-uint32_t my_htonl(uint32_t n) {
-	return (n & 0xFF000000) >> 24 | (n & 0x00FF0000) >> 8 | (n & 0x0000FF00) << 8 | (n & 0x000000FF) << 24; 
-}
-void  write_0x12345678() {
-	uint8_t network_buffer[] = { 0x12, 0x34, 0x56, 0x78 };
-	uint32_t* p = reinterpret_cast<uint32_t*>(network_buffer);
-	uint32_t n = htonl(*p); //my_htonl(*p); // TODO
-	printf("32 bit number=0x%x\n", n);
-}
+	
 
-int main() {
-	//write_4660();
-	write_0x1234();
-	write_0x12345678();
+	return 0;
 }
